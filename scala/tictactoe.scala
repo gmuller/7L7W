081204f4bfede7120e014741c70b220541db6a02
Grant Muller <grantmuller@gmail.com>
2012-08-13 15:38:14 -0400

    tic tac toe and scala day 1 complete

--- scala/tictactoe.scala
+++ scala/tictactoe.scala
@@ -1,6 +1,6 @@
 class TicTacToe(board: String = "_________") {
-	var gameBoard = Array.tabulate(9)((i) => board.charAt(i))
-	var winner = Nil
+	val gameBoard = Array.tabulate(9)((i) => board.charAt(i))
+	var winner : Char = '?'
 	var currentMove = 'O'
 	
 	def putMarker(index: Int) : Boolean = {
@@ -8,14 +8,35 @@ class TicTacToe(board: String = "_________") {
 		{
 			this.gameBoard(index) = currentMove
 			currentMove = if (currentMove == 'X') 'O' else 'X'
+			
+			//check cols
+			(0 until 2) foreach {i =>
+				val row = Set(gameBoard(i), gameBoard(i+3), gameBoard(i+6))
+				if (!row.contains('_') && row.size == 1) winner = row.head
+			}
+			
+			//check rows
+			(0 until 6 by 3) foreach {i =>
+				val row = Set(gameBoard(i), gameBoard(i+1), gameBoard(i+2))
+				if (!row.contains('_') && row.size == 1) winner = row.head
+			}
+			
+			val diag1 = Set(gameBoard(0), gameBoard(4), gameBoard(8))
+			if (!diag1.contains('_') && diag1.size == 1) winner = diag1.head
+			
+			val diag2 = Set(gameBoard(2), gameBoard(4), gameBoard(6))
+			if (!diag2.contains('_') && diag2.size == 1) winner = diag2.head
+			
+			if (!gameBoard.contains('_')) winner = 'N';
 			return true
 		}
 		return false
 	}
 	
 	def printBoard {
-		for (c <- this.gameBoard) {
-			print(c)
+		for (c <- 0 until this.gameBoard.size) {
+			if (c % 3 == 0 && c != 0) println
+			print(gameBoard(c) + " ")
 		}
 		println
 	}
@@ -24,13 +45,16 @@ class TicTacToe(board: String = "_________") {
 val ttt = new TicTacToe()
 println("Let's Get Started")
 
-while (ttt.winner == Nil) {
-	println("Here's the board: ")
+while (ttt.winner == '?') {
+	println("\nHere's the board: ")
 	ttt.printBoard
-	println(ttt.currentMove + "'s move.")
+	println("\n" + ttt.currentMove + "'s move.")
 	
 	val index = Console.readInt()
 	if (!ttt.putMarker(index)) {
-		println("Invalid Move")
+		println("\nInvalid Move!")
 	}
 }
+
+println("\nGame Over!")
+if (ttt.winner == 'N') println("Nobody Ever Wins") else println("Winner is: " + ttt.winner)
